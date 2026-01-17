c598c35059406a092bb1eb9fc9abd9053ac07636
DAVEJ-RENOVO\Administrator <davej99@dainit.com>
2026-01-17 17:31:58 +0900

    Convert short PHP tags to full tags in inc_head.php for compatibility with PHP 7+

--- sinc/skin/inc_head.php
+++ sinc/skin/inc_head.php
@@ -1,7 +1,7 @@
 <table border="0" cellpadding="0" cellspacing="0" width="1004" bgcolor="white" style="line-height:100%; margin-top:0; margin-bottom:0;">
 	<tr>
 		<td width="1250">
-			<p style="line-height:100%; margin-top:0; margin-bottom:0;"><? if($_SESSION['seUid'] && $_SESSION['seUserid']) { //로그인이 되어 있으면 ?><script src="/swf/index.php?src=sub1-1&width=1004&height=142"></script><? } else { //로그인이 되어있지 않으면 ?><script src="/swf/index.php?src=sub1&width=1004&height=142"></script><? } // end if ?>
+			<p style="line-height:100%; margin-top:0; margin-bottom:0;"><?php if($_SESSION['seUid'] && $_SESSION['seUserid']) { //로그인이 되어 있으면 ?><script src="/swf/index.php?src=sub1-1&width=1004&height=142"></script><?php } else { //로그인이 되어있지 않으면 ?><script src="/swf/index.php?src=sub1&width=1004&height=142"></script><?php } // end if ?>
 			</p>
 		</td>
 	</tr>
